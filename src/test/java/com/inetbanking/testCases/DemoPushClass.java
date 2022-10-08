50de18424ded6da4e93a7c40d234b320f0692bf2
Monikagatkal <monikagatkal43@gmail.com>
2022-10-08 16:28:11 +0530

    second commit

--- src/test/java/com/inetbanking/testCases/DemoPushClass.java
+++ src/test/java/com/inetbanking/testCases/DemoPushClass.java
@@ -1,5 +1,9 @@
 package com.inetbanking.testCases;
 
 public class DemoPushClass {
-  System.out.println("DemoPushclass ");
+  
+  private void abc() {
+	// TODO Auto-generated method stub
+	  System.out.println("DemoPushclass ");
+}
 }
